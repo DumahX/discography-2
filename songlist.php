d1454d34102b578299a61a7fddfbf3c99b777c85
DumahX <32107915+DumahX@users.noreply.github.com>
2019-04-25 13:40:49 -0500

    Two main pages for user access

---
+++ songlist.php
@@ -0,0 +1,20 @@
+<!-- sources
+https://en.wikipedia.org/wiki/Big_Bang_(South_Korean_band)-->
+<!doctype html>
+<html lang="en">
+<?php
+    // changing nav elements
+    $nav = false;
+    // change stylesheet
+    $style = "css/song2.css";
+    // establish title name for songlist page
+    $title = "BIGBANG Discography - Songlist";
+    // include paths
+    require_once('db_connect.php');
+    include('inc/header.php');
+    include('inc/content.php');
+    include('inc/footer.php');
+    include('inc/scripts.php');
+?>           
+    </body>
+</html>
